34677197ec0277121c7f9762f0c3d487509d21fa
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-01-20 16:41:38 -0300

    PUT client

--- src/main/java/com/rodrigo/desafiocrud/controllers/ClientController.java
+++ src/main/java/com/rodrigo/desafiocrud/controllers/ClientController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -36,5 +35,11 @@ public class ClientController {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
